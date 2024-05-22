ad13b30c6bb5ee40b87d72a6ff4894381d81b6fc
Tzune <91198340+Tzune85@users.noreply.github.com>
2024-05-23 00:59:40 +0200

    WIP

---
+++ mappa.js
@@ -0,0 +1,67 @@
+// Inizializza la mappa
+var mappa = L.map('mappa', {
+    minZoom: -1,
+    maxZoom: 1,
+    center: [0, 0],
+    zoom: -1,
+    crs: L.CRS.Simple,
+    doubleClickZoom: false
+});
+
+// Crea un'icona personalizzata
+var iconaPersonalizzata = L.icon({
+    iconUrl: './corvo_steam2.jpeg', // Sostituisci con l'URL della tua immagine
+    iconSize: [50, 50], // Sostituisci con le dimensioni della tua immagine
+});
+
+var tesoro = L.icon({
+    iconUrl: './tesoro.jpeg',
+    iconSize: [30, 30]
+})
+
+// Calcola le dimensioni della tua immagine
+var w = 1241; // larghezza dell'immagine
+var h = 1749;  // altezza dell'immagine
+
+// Imposta i limiti della mappa in base alle dimensioni dell'immagine
+mappa.setMaxBounds(new L.LatLngBounds([
+    [-h, -w],
+    [h, w]
+]));
+
+// Aggiungi la tua immagine come layer della mappa
+L.imageOverlay('./mappa.jpg', [[-h, -w], [h, w]]).addTo(mappa);
+
+// Aggiungi un marker che utilizza l'icona personalizzata
+var marker = L.marker([0, 0], {
+    icon: iconaPersonalizzata,
+    draggable: true
+}).addTo(mappa);
+
+
+// Definisci cambiaModalita come una funzione globale
+window.cambiaModalita = function() {
+    cancellazione = !cancellazione;
+    // Aggiorna il testo del pulsante
+    document.getElementById('pulsante').innerText = cancellazione ? 'Modalità: Cancellazione' : 'Modalità: Aggiunta';
+}
+
+// Aggiungi un gestore di eventi per i clic sulla mappa
+mappa.on('click', function(e) {
+    // Crea un nuovo marker al punto cliccato
+    var marker = L.marker(e.latlng, {
+        icon: tesoro,
+        draggable: true
+    }).addTo(mappa);
+
+    // Aggiungi un gestore di eventi per i clic del mouse sul marker
+    marker.on('mousedown', function(e) {
+        if (e.originalEvent.altKey) {
+            // Se il tasto Alt è premuto, rimuovi il marker dalla mappa
+            mappa.removeLayer(this);
+        }
+    });
+});
+
+
+
---
+++ pesca_carta.js
@@ -0,0 +1,40 @@
+document.getElementById('pescaButton').addEventListener('click', function() {
+    var semi = {
+        'Cuori': '<span class="seme cuori">♥</span>',
+        'Quadri': '<span class="seme quadri">♦</span>',
+        'Fiori': '<span class="seme fiori">♣</span>',
+        'Picche': '<span class="seme picche">♠</span>'
+    };
+    var valori = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Fante', 'Regina', 'Re', 'Asso'];
+    var mazzo = Object.entries(semi).flatMap(([nome, simbolo]) => valori.map(valore => valore + " di " + simbolo));
+
+    for (var i = 0; i < 4; i++) {
+        mazzo.push('Jolly')
+    }
+    var cartaPescata = mazzo[Math.floor(Math.random() * mazzo.length)];
+    document.getElementById('cartaPescata').innerHTML = "Hai pescato il " + cartaPescata;
+
+    // Crea un elemento div temporaneo e imposta il suo innerHTML come cartaPescata
+    var divTemp = document.createElement('div');
+    divTemp.innerHTML = cartaPescata;
+
+    // Ottieni solo il testo contenuto all'interno dell'elemento div (senza gli elementi span)
+    var cartaPescataSenzaSpan = divTemp.textContent || divTemp.innerText;        
+    
+    var cimitero = document.getElementById('cimitero')
+    var cartePescate = cimitero.value.split('\n'); // Ottieni tutte le carte pescate come un array
+    // Aggiungi la nuova carta all'inizio dell'array
+    cartePescate.unshift(cartaPescataSenzaSpan);
+    // Se ci sono più di 10 carte, rimuovi la carta più vecchia
+    if (cartePescate.length > 10) {
+        cartePescate.pop();
+    }
+    // Aggiorna il cimitero con le ultime 10 carte pescate
+    cimitero.value = cartePescate.join('\n');
+    function aggiornaAltezza() {
+    cimitero.style.height = 'auto';
+    cimitero.style.height = cimitero.scrollHeight + 'px';
+    }
+    cimitero.addEventListener('input', aggiornaAltezza);
+    aggiornaAltezza();
+    }) 
